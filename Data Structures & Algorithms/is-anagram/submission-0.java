8572b673cba9cb0f84c8982bf78ee98bcf9727a6
REETRISHA PRAMANICK <reetrishapramanick@gmail.com>
2026-09-25 13:01:29 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] hash = new int[26];
+        for(int i = 0; i<s.length(); i++)
+        {
+            hash[s.charAt(i) - 'a']++; //keep track of the letters in first word
+        }
+        for(int i = 0; i<t.length(); i++)
+        {
+            hash[t.charAt(i) - 'a']--; //the letters cancel out each other
+        }
+        for(int i = 0; i<26; i++)
+        {
+            if(hash[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
